6f62268f08afb84cc9d796702a7761f8f49b414b
ynushi <yutaro.nushi@teradata.com>
2025-05-22 16:23:31 +0900

    test: テスト項目を削除

--- src/test_app.py
+++ src/test_app.py
@@ -7,7 +7,7 @@ from app import add
     (-5, -5, -10),   # ケース3
     (0, 100, 100),   # ケース4
     (100, 100, 200),   # ケース6
-    (1, 2, 3),       # ケース5: デモ用の失敗ケース
+    # (1, 2, 3),       # ケース5: デモ用の失敗ケース
 ])
 def test_add_parametrized(input_a, input_b, expected_output):
   """パラメータ化されたadd関数のテスト"""
